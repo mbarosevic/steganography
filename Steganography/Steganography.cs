8a054ad55619a97c41c7f3c96112251a2283f8d9
Mattheeeew <44161476+Mattheeeew@users.noreply.github.com>
2020-04-24 22:42:44 +0200

    Document Steganography class

--- Steganography/Steganography.cs
+++ Steganography/Steganography.cs
@@ -3,13 +3,19 @@ using System.Drawing;
 
 namespace Steganography
 {
+    /// <summary>
+    /// Steganography class that encode and decode text in selected image 
+    /// </summary>
     class Steganography
     {
-        public Steganography()
-        {
+        public Steganography() { }
 
-        }
-
+        /// <summary>
+        /// Encode method that hide the given text in given bitmap image
+        /// </summary>
+        /// <param name="bitmap"></param>
+        /// <param name="textToEncode"></param>
+        /// <returns>bitmap image with hidden text</returns>
         public Bitmap Encode(Bitmap bitmap, string textToEncode)
         {
             Console.WriteLine(textToEncode);
@@ -21,15 +27,19 @@ namespace Steganography
                     Color pixel = nonIndexedBitmap.GetPixel(i, j);
                     if (i < 1 && j < textToEncode.Length)
                     {
-                        //TEST
                         /*
+                         * Uncomment lines below to check the values of pixels in which every letter is saved
+                         * 
                         Console.WriteLine("R= [" + i + "][" + j + "]=" + pixel.R);
                         Console.WriteLine("G= [" + i + "][" + j + "]=" + pixel.G);
                         Console.WriteLine("B= [" + i + "][" + j + "]=" + pixel.B);
                         */
                         char letter = Convert.ToChar(textToEncode.Substring(j, 1));
                         int value = Convert.ToInt32(letter);
+
+                        //Compare the letter with pixel value
                         //Console.WriteLine("letter :" + letter + " value :" + value);
+                        
                         nonIndexedBitmap.SetPixel(i, j, Color.FromArgb(pixel.R, pixel.G, value));
                     }
                     if (i == nonIndexedBitmap.Width - 1 && j == nonIndexedBitmap.Height - 1)
@@ -41,6 +51,12 @@ namespace Steganography
             return nonIndexedBitmap;
         }
 
+        /// <summary>
+        /// Create non indexed bitmap method which returns a non indexed bitmap image
+        /// in case of selected indexed image by user
+        /// </summary>
+        /// <param name="indexedBitmap"></param>
+        /// <returns>non indexed image bitmap</returns>
         public Bitmap CreateNonIndexedBitmap(Bitmap indexedBitmap)
         {
             Bitmap nonIndexedBitmap = new Bitmap(indexedBitmap.Width, indexedBitmap.Height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
@@ -53,6 +69,11 @@ namespace Steganography
             return nonIndexedBitmap;
         }
 
+        /// <summary>
+        /// Decode method that unhide hidden text in given bitmap image
+        /// </summary>
+        /// <param name="bitmap"></param>
+        /// <returns>hidden text in bitmap image</returns>
         public string Decode(Bitmap bitmap)
         {
             string hiddenMessage = "";
@@ -71,10 +92,8 @@ namespace Steganography
                         String letter = System.Text.Encoding.ASCII.GetString(new byte[] { Convert.ToByte(c) });
                         //TEST
                         //Console.WriteLine($"letter: {letter}   value {value}");
-                        if(letter != "?")
-                        {
-                            hiddenMessage = hiddenMessage + letter;
-                        }
+                        
+                        hiddenMessage = hiddenMessage + letter;
                     }
                 }
             }
