6601af209bb1f32c0b0d6ce62be9e6e048c5948c
Mattheeeew <44161476+Mattheeeew@users.noreply.github.com>
2020-04-27 18:03:26 +0200

    Implement Decryption method in AesManaged class

--- Steganography/AesManaged.cs
+++ Steganography/AesManaged.cs
@@ -49,5 +49,47 @@ namespace Steganography
             }
         }
 
+        public string DecryptString(string cipherText, byte[] key, byte[] iv)
+        {
+            using (Aes encryptor = Aes.Create())
+            {
+                encryptor.Mode = CipherMode.CBC;
+
+                byte[] aesKey = new byte[32];
+                Array.Copy(key, 0, aesKey, 0, 32);
+                encryptor.Key = aesKey;
+                encryptor.IV = iv;
+
+                MemoryStream memoryStream = new MemoryStream();
+                ICryptoTransform aesDecryptor = encryptor.CreateDecryptor();
+                CryptoStream cryptoStream = new CryptoStream(memoryStream, aesDecryptor, CryptoStreamMode.Write);
+
+                string plainText = String.Empty;
+                try
+                {
+                    // Convert the ciphertext string into a byte array
+                    byte[] cipherBytes = Convert.FromBase64String(cipherText);
+
+                    // Decrypt the input ciphertext string
+                    cryptoStream.Write(cipherBytes, 0, cipherBytes.Length);
+
+                    // Complete the decryption process
+                    cryptoStream.FlushFinalBlock();
+
+                    // Convert the decrypted data from a MemoryStream to a byte array
+                    byte[] plainBytes = memoryStream.ToArray();
+
+                    // Convert the decrypted byte array to string
+                    plainText = Encoding.ASCII.GetString(plainBytes, 0, plainBytes.Length);
+                }
+                finally
+                {
+                    memoryStream.Close();
+                    cryptoStream.Close();
+                }
+
+                return plainText;
+            }
+        }
     }
 }
